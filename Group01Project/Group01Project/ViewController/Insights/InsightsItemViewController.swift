dba09272fe0c489401ec1f54f9fe44b978ebce28
Bowie <bowie.tso.p.y@gmail.com>
2019-11-25 11:58:54 +0800

    added insight tabBar

--- Group01Project/Group01Project/ViewController/Insights/InsightsItemViewController.swift
+++ Group01Project/Group01Project/ViewController/Insights/InsightsItemViewController.swift
@@ -9,16 +9,46 @@
 import UIKit
 
 class InsightsItemViewController: BaseViewController  {
+    
+    @IBOutlet weak var insightButton: UIButton!
+    @IBOutlet weak var pressButton: UIButton!
+    
     var rootRouter: RootRouter? {
       return router as? RootRouter
     }
+    
+    var tabStatus : Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        uiBind()
         // Do any additional setup after loading the view.
     }
+    
+    func uiBind(){
+        if tabStatus{
+            insightButton.isUserInteractionEnabled = false
+            pressButton.isUserInteractionEnabled = true
+            insightButton.backgroundColor = UIColor.init(red: 74, green: 144, blue: 226)
+            pressButton.backgroundColor = UIColor.init(red: 190, green: 190, blue: 190)
+        }else{
+            pressButton.backgroundColor = UIColor.init(red: 74, green: 144, blue: 226)
+            insightButton.backgroundColor = UIColor.init(red: 190, green: 190, blue: 190)
+            insightButton.isUserInteractionEnabled = true
+            pressButton.isUserInteractionEnabled = false
+        }
+        
+    }
 
-    @IBAction func buttonClicked(_ sender: Any) {
-        rootRouter?.showTest()
+    @IBAction func insightClicked(_ sender: Any) {
+        
+        tabStatus.toggle()
+        uiBind()
+    }
+    
+    @IBAction func pressClicked(_ sender: Any) {
+        
+        tabStatus.toggle()
+        uiBind()
     }
 }
