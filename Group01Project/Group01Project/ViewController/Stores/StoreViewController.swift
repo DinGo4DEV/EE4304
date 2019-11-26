5e352790f3ee0cb4265bc7af4b49c9837d076ff7
Neptune Wong <tecowong1@gmail.com>
2019-11-26 19:29:13 +0800

    no message

--- Group01Project/Group01Project/ViewController/Stores/StoreViewController.swift
+++ Group01Project/Group01Project/ViewController/Stores/StoreViewController.swift
@@ -29,6 +29,9 @@ class StoreViewController: BaseViewController,CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        place.isHidden = true
+        
         DispatchQueue.main.async {
             if(BaseViewController.loaded){
                 self.stopLoading()
@@ -52,8 +55,24 @@ class StoreViewController: BaseViewController,CLLocationManagerDelegate {
         
         // Do any additional setup after loading the view.
     }
-    @IBAction func buttonTapped(_ sender: UIButton){
-        print("button tapped")
+    @objc func buttonAction(sender: UIButton!){
+        if place.isHidden {
+            animate(toogle: true)
+        }else{
+            animate(toogle: false)
+        }
+    }
+    
+    func animate(toogle: Bool) {
+        if toogle{
+            UIView.animate(withDuration: 0.3) {
+                self.place.isHidden = false
+            }
+        }else{
+            UIView.animate(withDuration: 0.3) {
+                self.place.isHidden = true
+            }
+        }
     }
     
     override func loadView() {
@@ -63,34 +82,36 @@ class StoreViewController: BaseViewController,CLLocationManagerDelegate {
         //for button
         let button = UIButton()
         button.frame = self.viewForButton.frame
-
+        
         // Set the font of the button text
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
-
+        
         // The default text of the button
         button.setTitle("Places", for: .normal)
-
+        
         // The text that will appear when the button is tapped
         button.setTitle("I am being tapped", for: .highlighted)
         
         // The default color of the button text
         button.setTitleColor(UIColor.black, for: .normal)
-
+        
         // The color of the button text when the button is tapped
         button.setTitleColor(UIColor.black, for: .highlighted)
         button.layer.zPosition = 100
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
         
         self.view.addSubview(button)
-    
+        
         //for map
         let camera = GMSCameraPosition.camera(withLatitude: 22.3700556, longitude: 114.1535941, zoom: 11.0)
-
+        
         mapView = GMSMapView.map(withFrame:CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height), camera: camera)
         mapView.isMyLocationEnabled = true
         mapView.settings.myLocationButton = true
         mapView.center = self.view.center
-//        mapView.padding = UIEdgeInsets(top: 0, left: 0, bottom: 50, right: 50)
-//        view = mapView
+        //        mapView.padding = UIEdgeInsets(top: 0, left: 0, bottom: 50, right: 50)
+        //        view = mapView
         viewForButton.layer.zPosition = 100
         
         view.addSubview(mapView)
@@ -124,6 +145,11 @@ extension StoreViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        loadView().button.setTitle("PlaceList[indexPath.row]", for: .normal)
+        animate(toogle: false)
+    }
+    
     
     // Handle incoming location events.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -145,17 +171,17 @@ extension StoreViewController: UITableViewDelegate, UITableViewDataSource {
     // Handle authorization for the location manager.
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         switch status {
-            case .restricted:
-                print("Location access was restricted.")
-            case .denied:
-                print("User denied access to location.")
-                // Display the map using the default location.
-                mapView.isHidden = false
-            case .notDetermined:
-                print("Location status not determined.")
-            case .authorizedAlways: fallthrough
-            case .authorizedWhenInUse:
-                print("Location status is OK.")
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
         }
     }
     
